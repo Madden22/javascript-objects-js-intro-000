6fc42bdcfdf49a307b200e45c8366d283cb5bd9a
Sydney Michael <m13s19madden@gmail.com>
2020-03-10 21:51:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist2, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist2, artistName){
-  delete playlist2.artistName
+  delete playlist2[artistName]
   return playlist2
 }
