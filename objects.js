6c57055bbbfcfde0bba4d13423f7305fa70e2113
Sydney Michael <m13s19madden@gmail.com>
2020-03-10 19:02:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,4 +4,10 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign(playlist, {artistName : songTitle})
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+  return playlist
 }
